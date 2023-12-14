1eb063b3e2c7397d57523e1d5741b096d7eccc2a
66996 <66996@HAQ0>
2023-12-14 18:04:53 +0300

    Day 09

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -5,41 +5,42 @@ var steps = data[0];
 
 var route = data.Skip(2).ToArray();
 Dictionary<string, (string Left, string Right)> routes = new Dictionary<string, (string Left, string Right)>();
+List<string> endWithCharA = new List<string>();
 
 foreach (var s in route)
 {
     routes.Add(s[..3], (s[7..10],s[12..15]));
+    
+    if(s[2] is 'A') endWithCharA.Add(s);
 }
 
-const string reach = "ZZZ";
-const string start = "AAA";
+// const string reach = "ZZZ";
+// const string start = "AAA";
 int stepCount = 0;
 int totalSteps = 0;
 
-string dest = start;
+string[] destinations = endWithCharA.Select(s => s[..3]).ToArray();
+
+
+bool[] reaches = new bool[endWithCharA.Count];
 
 while (true)
 {
-    if (CheckRoute(dest))
+    
+    for (int i = 0; i < destinations.Length; i++)
     {
-        break;
+        reaches[i] = CheckRoute(destinations[i],i);
     }
-}
+    totalSteps++;
+    stepCount = (stepCount + 1) % steps.Length;
 
+    if (reaches.All(b => b)) break;
+}
 
 Console.WriteLine(totalSteps);
 
-bool CheckRoute(string destination)
+bool CheckRoute(string destination, int destIndex)
 {
-    dest = steps[stepCount] is 'L' ? routes[destination].Left : routes[destination].Right;
-    stepCount = (stepCount + 1) % steps.Length;
-    totalSteps++;
-
-    if (dest is reach)
-    {
-        return true;
-    }
-
-    return false;
+    destinations[destIndex] = steps[stepCount] is 'L' ? routes[destination].Left : routes[destination].Right;
+    return destinations[destIndex][2] is 'Z';
 }
-
---
+++ Day09/Program.cs
@@ -0,0 +1,70 @@
+﻿
+var testInput = File.ReadAllLines("input.txt");
+
+
+
+var input = Enumerable.Range(0, testInput.Length).Select((i => testInput[i].Split(' '))).ToArray();
+
+Stack<List<int>> increment = new Stack<List<int>>();
+int incrementSize = 0;
+
+int sequencePrediction = 0;
+int sumSequencePredictions = 0;
+foreach (var i in input)
+{
+    increment.Clear();
+    increment.Push(Array.ConvertAll(i, int.Parse).ToList());
+    
+    while(true)
+    {
+        var usage = increment.Count == 1 ? Array.ConvertAll(i, s => int.Parse(s)) : increment.Peek().ToArray();
+        
+        incrementSize = usage.Length - 1;
+
+        var incList = new List<int>();
+        for (int j = 0; j < incrementSize; j++)
+        {
+            incList.Add(0);
+        }
+        
+    
+        for (var j = 0; j < incList.Count; j++)
+        {
+            incList[j] = usage[j + 1] - usage[j];
+        }
+        increment.Push(incList);
+
+        if (incList.All(inc => inc == 0)) break;
+    }
+
+    bool firstAdded = false;
+    
+
+    while (true)
+    { 
+        var incList = increment.Pop();
+        var upperIncList = increment.Peek();
+        if(!firstAdded) 
+        {
+            // incList.Add(incList[0]);
+            incList.Insert(0, incList[0]);
+            firstAdded = true;
+        }
+        
+        // upperIncList.Add(incList[^1] + upperIncList[^1]);
+        upperIncList.Insert(0,upperIncList[0] -  incList[0]);
+
+        if (increment.Count == 1)
+        {
+            // sequencePrediction = upperIncList[^1];
+            sequencePrediction = upperIncList[0];
+            break;
+        }
+    }
+
+    Console.WriteLine(sequencePrediction);
+    
+    sumSequencePredictions += sequencePrediction;
+}
+
+Console.WriteLine("sum = " + sumSequencePredictions);
