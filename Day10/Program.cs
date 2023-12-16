5828e10fdaf64ab7df6e497f0ccf3451f59425fe
66996 <66996@HAQ0>
2023-12-16 15:16:58 +0300

    Day 10 part 2

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -10,87 +10,127 @@ const char start = 'S';
 
 var input = File.ReadAllLines("input.txt");
 
-char[,] loopTable = new char[input.Length,input.Length];
-string[,] loopsDistanceTable = new string[input.Length, input.Length];
+char[][] loopTable = new char[input.Length][];
+char[][] loopP2Table = new char [input.Length][];
+string[][] loopsDistanceTable = new string[input.Length][];
 
 for (var i = 0; i < input.Length; i++)
 {
-    for (int j = 0; j < input[i].Length; j++)
+    loopTable[i] = new char[input[0].Length];
+    loopP2Table[i] = new char[input[0].Length];
+    loopsDistanceTable[i] = new string[input[0].Length];
+    for (var j = 0; j < input[i].Length; j++)
     {
-        loopTable[i, j] = input[i][j];
-        loopsDistanceTable[i, j] = input[i][j].ToString();
+        loopTable[i][j] = input[i][j];
+        
+        loopsDistanceTable[i][j] = input[i][j].ToString();
 
-        if (loopTable[i, j] == start)
+        if (loopTable[i][j] == start)
         {
             startPos = (i, j);
-            loopsDistanceTable[i, j] = 0.ToString();
+            loopsDistanceTable[i][j] = "0";
         }
     }
 }
 
-int loopTableWidth = loopTable.GetLength(0);
-int loopTableHeight = loopTable.GetLength(1);
+int loopTableWidth = loopTable[0].Length;
+int loopTableHeight = loopTable.GetLength(0);
 int distanceCounter = 1;
 List<(int, int)> lastCheckedElements = new List<(int, int)>();
 List<(int, int)> checkingElements = new List<(int, int)>() { (startPos.i, startPos.j) };
-List<(int, int)> tempCheckingElemenents = new List<(int, int)>();
+List<(int, int)> tempCheckingElements = new List<(int, int)>();
 
 CheckingLoop();
+
+
+int exclCount = 0;
+int insideCount = 0;
+
+for (int i = 0; i < loopP2Table.GetLength(0); i++)
+{
+    for (int j = 0; j < loopP2Table[i].Length; j++)
+    {
+        loopP2Table[i][j] = (input[i][j] is verticalPipe or NEPipe or NWPipe) && lastCheckedElements.Contains((i,j)) ? '!' : input[i][j];
+        exclCount = 0;
+        for (var k = 0; k < loopP2Table[i].Length; k++)
+        {
+            if (loopP2Table[i][j] is '!' or 'S') continue;
+            if (loopP2Table[i][k] is not '!') continue;
+
+            if (k < j && !lastCheckedElements.Contains((i,j)))
+            {
+                exclCount++;
+            }
+        }
+
+        if (exclCount % 2 is 1)
+        {
+            Console.WriteLine((i,j));
+            loopsDistanceTable[i][j] = "I";
+            insideCount++;
+        }
+        
+        
+    }
+}
+
 for (int i = 0; i < loopsDistanceTable.GetLength(0); i++)
 {
-    for (int j = 0; j < loopsDistanceTable.GetLength(1); j++)
+    for (int j = 0; j < loopsDistanceTable[i].Length; j++)
     {
-        Console.Write(loopsDistanceTable[i,j]);
+        Console.Write(loopsDistanceTable[i][j]);
     }
     Console.Write("\n");
 }
 Console.Write("\n");
-Console.WriteLine(loopsDistanceTable[tempCheckingElemenents[0].Item1,tempCheckingElemenents[0].Item2]);
+
+// Console.WriteLine(loopsDistanceTable[tempCheckingElements[0].Item1,tempCheckingElements[0].Item2]);
+Console.WriteLine(insideCount);
 void CheckingLoop()
 {
     while (checkingElements.Count > 0)
     {
-        tempCheckingElemenents.Clear();
-        tempCheckingElemenents.AddRange(checkingElements);
+        tempCheckingElements.Clear();
+        tempCheckingElements.AddRange(checkingElements);
         checkingElements.Clear();
         
-        foreach ((int xIndex,int yIndex) in tempCheckingElemenents)
+        foreach ((int xIndex,int yIndex) in tempCheckingElements)
         {
             //Checking right
-            if (yIndex + 1 < loopTableWidth && GetCondition(loopTable[xIndex,yIndex], loopTable[xIndex, yIndex + 1], Direction.East))
+            if (yIndex + 1 < loopTableWidth && GetCondition(loopTable[xIndex][yIndex], loopTable[xIndex][yIndex + 1], Direction.East))
             {
                 CheckingNeighbour(xIndex,
                     yIndex + 1,
-                    loopTable[xIndex, yIndex + 1] == horizontalPipe ||
-                    loopTable[xIndex, yIndex + 1] == NWPipe ||
-                    loopTable[xIndex, yIndex + 1] == SWPipe);
+                    loopTable[xIndex][yIndex + 1] == horizontalPipe ||
+                    loopTable[xIndex][yIndex + 1] == NWPipe ||
+                    loopTable[xIndex][yIndex + 1] == SWPipe);
             }
             //Checking left
-            if (yIndex - 1 >= 0 && GetCondition(loopTable[xIndex,yIndex], loopTable[xIndex, yIndex - 1], Direction.West))
+            if (yIndex - 1 >= 0 && GetCondition(loopTable[xIndex][yIndex], loopTable[xIndex][yIndex - 1], Direction.West))
             {
                 CheckingNeighbour(xIndex,
                     yIndex - 1,
-                    loopTable[xIndex, yIndex - 1] == horizontalPipe ||
-                    loopTable[xIndex, yIndex - 1] == NEPipe ||
-                    loopTable[xIndex, yIndex - 1] == SEPipe);
+                    loopTable[xIndex][yIndex - 1] == horizontalPipe ||
+                    loopTable[xIndex][yIndex - 1] == NEPipe ||
+                    loopTable[xIndex][yIndex - 1] == SEPipe);
             }
             //Checking up
-            if (xIndex - 1 >= 0 && GetCondition(loopTable[xIndex,yIndex], loopTable[xIndex - 1, yIndex], Direction.North))
+            if (xIndex - 1 >= 0 && GetCondition(loopTable[xIndex][yIndex], loopTable[xIndex - 1][yIndex], Direction.North))
             {
                 CheckingNeighbour(xIndex - 1,
                     yIndex,
-                    loopTable[xIndex - 1, yIndex] == verticalPipe ||
-                    loopTable[xIndex - 1, yIndex] == SWPipe ||
-                    loopTable[xIndex - 1, yIndex] == SEPipe);
+                    loopTable[xIndex - 1][yIndex] == verticalPipe ||
+                    loopTable[xIndex - 1][yIndex] == SWPipe ||
+                    loopTable[xIndex - 1][yIndex] == SEPipe);
             }
             //Checking down
-            if (xIndex + 1 < loopTableHeight && GetCondition(loopTable[xIndex,yIndex], loopTable[xIndex + 1, yIndex], Direction.South))
+            if (xIndex + 1 < loopTableHeight && GetCondition(loopTable[xIndex][yIndex], loopTable[xIndex + 1][yIndex], Direction.South))
             {
                 CheckingNeighbour(xIndex + 1,
                     yIndex,
-                    loopTable[xIndex + 1, yIndex] == verticalPipe ||
-                    loopTable[xIndex + 1, yIndex] == NEPipe ||
-                    loopTable[xIndex + 1, yIndex] == NWPipe);
+                    loopTable[xIndex + 1][yIndex] == verticalPipe ||
+                    loopTable[xIndex + 1][yIndex] == NEPipe ||
+                    loopTable[xIndex + 1][yIndex] == NWPipe);
             }
             
         }
@@ -115,8 +155,8 @@ void CheckingNeighbour(int xIndex, int yIndex, bool pipeCondition)
         {
             checkingElements.Add((xIndex,yIndex));
         }
-            
-        loopsDistanceTable[xIndex, yIndex] = distanceCounter.ToString();
+
+        // loopsDistanceTable[xIndex][yIndex] = distanceCounter.ToString();
             
         lastCheckedElements.Add((xIndex,yIndex));
     }
@@ -124,7 +164,10 @@ void CheckingNeighbour(int xIndex, int yIndex, bool pipeCondition)
 
 bool GetCondition(char ownChar, char neighbourChar, Direction direction)
 {
-    if (ownChar is start) return true;
+    if (ownChar is start)
+    {
+        return true;
+    }
     
     switch (direction)
     {
