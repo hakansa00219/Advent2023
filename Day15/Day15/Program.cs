f666c234e125295ede3e24800707a008d27a7162
66996 <66996@HAQ0>
2023-12-20 16:53:44 +0300

    Day 15 part 2

--- Day15/Day15/Program.cs
+++ Day15/Day15/Program.cs
@@ -5,13 +5,24 @@ var input = File.ReadAllText("input.txt");
 var testInput = "HASH";
 var splittedInput = input.Split(',');
 
+Dictionary<byte, List<BoxData>> boxes = new Dictionary<byte, List<BoxData>>(); 
+
 int sum = 0;
 int currentValue = 0;
 
+
 foreach (var s in splittedInput)
 {
-    byte[] bytes = new byte[s.Length];
-    bytes = Encoding.ASCII.GetBytes(s);
+    var label = string.Concat(s.TakeWhile(c => c is not ('=' or '-')).TakeWhile(char.IsLetter));
+    
+    var operation = s.SkipWhile(char.IsLetter).First();
+    int lens = -1;
+    if (operation is '=')
+    {
+        lens = int.Parse(string.Concat(s.SkipWhile(char.IsLetter).Skip(1)));
+    }
+    byte[] bytes = new byte[label.Count()];
+    bytes = Encoding.ASCII.GetBytes(label);
 
     currentValue = 0;
     
@@ -22,9 +33,59 @@ foreach (var s in splittedInput)
         currentValue %= 256;
     }
 
-    Console.WriteLine(currentValue);
+    byte boxValue = Convert.ToByte(currentValue);
+    BoxData boxData = new BoxData() { s = label, v = lens };
+    if (operation == '=')
+    {
+        
+        if (!boxes.ContainsKey(boxValue))
+        {
+            boxes.Add(boxValue, new List<BoxData>());
+            boxes[boxValue].Add(boxData);
+        }
+        else
+        {
+            var foundData = boxes[boxValue].FirstOrDefault(box => box.s == boxData.s);
 
-    sum += currentValue;
+            if (foundData is null)
+            {
+                boxes[boxValue].Add(boxData);
+            }
+            else
+            {
+                foundData.v = lens;
+            }
+        }
+    }
+    else
+    {
+        if(!boxes.TryGetValue(boxValue, out var value)) continue;
+
+        var foundData = value.FirstOrDefault(box => box.s == boxData.s);
+
+        if (foundData is null) continue;
+        
+        value.Remove(foundData);
+        
+        if (!value.Any())
+        {
+            boxes.Remove(boxValue);
+        }
+    }
+}
+foreach (var (key, value) in boxes)
+{
+    foreach (var boxData in value)
+    {
+        int total = (key + 1) * (value.FindIndex(m => m.s == boxData.s) + 1) * boxData.v;
+        sum += total;
+        Console.WriteLine($"- {boxData.s}: {key + 1} * {value.FindIndex(m => m.s == boxData.s) + 1} * {boxData.v} = {total}");
+    }
 }
 
 Console.WriteLine(sum);
+class BoxData
+{
+    public string s;
+    public int v;
+}
